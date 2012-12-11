d60e4b67cee4f825adcdc831bfbfc4d9639fd224
Christian Gastrell <cgastrell@gmail.com>
2012-12-11 01:55:29 -0300

    editando pagina historia

--- javascripts/cube.js
+++ javascripts/cube.js
@@ -22,3 +22,41 @@ var coords = [
     {x: 0, y: 90},  // face5
     {x: 0, y: -90} // face3
 ];
+var testElement = document.createElement('div');
+var transformProperty = judge(testElement,  ['transform', 'WebkitTransform', 'MozTransform', 'OTransform']);
+var transitionDurationProperty = judge(testElement, ['transitionDuration', 'WebkitTransitionDuration',  'MozTransitionDuration', 'OTransitionDuration',  'msTransitionDuration']);
+    
+function judge(elm, props) {
+    for(var i = 0, l = props.length; i < l; i++) {
+        if(typeof elm.style[props[i]] !== "undefined") {
+            return props[i];
+        }
+    }
+}
+
+var  container = {
+    x: -10, 
+    y: 20, 
+    el: $('#cube')[0],
+    move: function(coords) {
+        if(coords) {
+            if(typeof coords.x === "number") this.x = coords.x;
+            if(typeof coords.y === "number") this.y = coords.y;
+        }
+
+        this.el.style[transformProperty] = "rotateX("+this.x+"deg) rotateY("+this.y+"deg)";
+    },
+    reset: function() {
+        this.move({x: 0, y: 0});
+    }
+};
+        
+container.duration = function() {
+    container.el.style[transitionDurationProperty] = "250ms";
+    return 250;
+}();
+
+$('a.jump').click(function(e){
+    var to_idx = $(this).attr('to');
+    container.move(coords[to_idx]);
+});
